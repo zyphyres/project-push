e0ae38882d9c8fec0b165f7f8fe1323919066b06
Zyphyres <mellomarsh686@gmail.com>
2024-10-17 14:38:45 +0800

    new

--- src/screens/landing/LandingPage.jsx
+++ src/screens/landing/LandingPage.jsx
@@ -228,7 +228,7 @@ const LandingPage = () =>
                       color: '#056a94'
                     }}}
                   >
-                    LOG IN
+                    TAEE
                   </Button>
                 </Center>   
               </form> 
