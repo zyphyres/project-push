2807345095ac1250414ee9da6d1c4fe46a44024d
Zyphyres <mellomarsh686@gmail.com>
2024-10-17 14:43:47 +0800

    ungo

--- src/screens/landing/LandingPage.jsx
+++ src/screens/landing/LandingPage.jsx
@@ -228,7 +228,7 @@ const LandingPage = () =>
                       color: '#056a94'
                     }}}
                   >
-                    UNGO
+                    UNGOss
                   </Button>
                 </Center>   
               </form> 
