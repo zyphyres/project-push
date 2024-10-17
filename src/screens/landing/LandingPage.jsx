5a3dbef3185d2320c55c1ea910f494530b3b6299
Zyphyres <mellomarsh686@gmail.com>
2024-10-17 14:49:22 +0800

    new

--- src/screens/landing/LandingPage.jsx
+++ src/screens/landing/LandingPage.jsx
@@ -228,7 +228,7 @@ const LandingPage = () =>
                       color: '#056a94'
                     }}}
                   >
-                    UNGOss
+                    LOGIN
                   </Button>
                 </Center>   
               </form> 
